744d8a450bb9cd912f929ba718d2ed50995a502b
prakhar pandey <prakhar9704@gmail.com>
2025-11-11 21:10:01 +0530

    added doctors pages

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import appointmentModel from "../models/appointmentModel.js";
 
 const loginDoctor=async(req,res)=>{
     try{
@@ -40,6 +41,87 @@ const loginDoctor=async(req,res)=>{
 }
 export {loginDoctor}
 
+// show all appointments of a doc on his dashboard
+
+const appointmentDoctor=async(req,res)=>{
+    try{
+        const {docId}=req.body;
+        const appointments=await appointmentModel.find({docId});
+
+        res.status(200).json({
+            success:true,
+            appointments
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+export {appointmentDoctor}
+
+// doctor can cancel his appointment
+
+const appointmentCancel=async(req,res)=>{
+    try{
+        const {docId,appointmentId}=req.body;
+        const appointmentData=await appointmentModel.findById(appointmentId);
+
+        if(appointmentData && appointmentData.docId===docId){
+            await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true});
+            return res.status(200).json({
+                success:true,
+                message:"Appointment cancelled"
+            })
+        }
+        return res.json({
+            success:false,
+            message:"cannot cancel appointment"
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+export {appointmentCancel}
+
+// doctor can mark any appointment as completed
+const appointmentComplete=async(req,res)=>{
+    try{
+        const {docId,appointmentId}=req.body;
+
+        const appointmentData=await appointmentModel.findById(appointmentId);
+
+        if(appointmentData && appointmentCancel.docId===docId){
+            await appointmentModel.findByIdAndUpdate(appointmentId,{isCompleted:true})
+
+            return res.json({
+                success:true,
+                message:"appointment completed"
+            })
+        }
+        return res.json({
+            success:false,
+            message:"appointment cant be completed"
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+export {appointmentComplete}
+
 const changeAvailability=async(req,res)=>{
     try{
         const {docId}=req.body;
@@ -60,7 +142,7 @@ const changeAvailability=async(req,res)=>{
         })
     }
 }
-
+// show all docs on frontend
 const doctorList=async(req,res)=>{
     try{
         const doctors=await doctorModel.find({}).select(['-password','-email']);
@@ -81,3 +163,44 @@ const doctorList=async(req,res)=>{
 }
 
 export {changeAvailability,doctorList}
+
+// show dashboard of a doc
+const doctorDashboard=async(req,res)=>{
+    try{
+        const {docId}=req.body
+        const appointments=await appointmentModel.find({docId})
+
+        let earnings=0;
+        appointments.map((item)=>{
+            if(item.isCompleted || item.payment){
+                earnings+=item.amount;
+            }
+        })
+        let patients=[]
+
+        appointments.map((item)=>{
+            if(!patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+
+        const dashData={
+            earnings,
+            appointments:appointments.length,
+            patients:patients.length,
+            latestAppointments:appointments.reverse()
+        }
+        return res.json({
+            success:true,
+            dashData
+        })
+    }
+    catch(error){
+        console.log(error)
+        return res.json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+export {doctorDashboard}
